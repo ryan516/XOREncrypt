8248b2a68980ed19ed0ec7522c256e9b662ff731
Ryan Smith <the4swordz@gmail.com>
2013-10-07 22:27:03 -0600

    Created initial program

---
+++ README.md
@@ -0,0 +1,3 @@
+==README
+
+I am a simple encryption program written in Python, converting your Ascii Character input into a Binary Value, and XORing it with your Key's Binary Values.
---
+++ enc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+import binascii
+def encrypt:
+	text = raw_input("Please input your information to encrypt: ")
+	text = text.split(,)
+	for char in text:
+		char = bin(int(binascii.hexlify(char),16))
+	key = raw_input("Please input your key for decryption: ")
+	key = key.split(,)
+	for char in key:
+		char = bin(int(binascii.hexlify(char),16))
+		
