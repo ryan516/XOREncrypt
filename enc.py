c6c0fe4f75f91c088f56cbee7076ebb254d56176
Ryan Smith <the4swordz@gmail.com>
2013-10-07 22:53:03 -0600

    Fixed bug on line #5

--- enc.py
+++ enc.py
@@ -2,11 +2,9 @@
 import binascii
 def encrypt():
 	text = raw_input("Please input your information to encrypt: ")
-	text = text.split(,)
 	for char in text:
 		char = bin(int(binascii.hexlify(char),16))
 	key = raw_input("Please input your key for decryption: ")
-	key = key.split(,)
 	for char in key:
 		char = bin(int(binascii.hexlify(char),16))
 
