dc08305d241f55cfbb846f5a1287f9d10e6c1925
Ryan Smith <the4swordz@gmail.com>
2013-10-07 22:50:21 -0600

    Fixed bug in Encryption

--- enc.py
+++ enc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 import binascii
-def encrypt:
+def encrypt():
 	text = raw_input("Please input your information to encrypt: ")
 	text = text.split(,)
 	for char in text:
@@ -9,4 +9,8 @@ def encrypt:
 	key = key.split(,)
 	for char in key:
 		char = bin(int(binascii.hexlify(char),16))
+
+	print key
+
+
 		
