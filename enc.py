135d7f0f716b2f6b59e9efa98ea01b41361c6041
Ryan Smith <the4swordz@gmail.com>
2013-10-07 23:34:04 -0600

    Fixed all known bugs.

--- enc.py
+++ enc.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python2
-import binascii
 def encrypt():
-	text = raw_input("Please input your information to encrypt: ")
-	for char in text:
-		#Convert text into a binary sequence
-		char = bin(int(binascii.hexlify(char),16))
-	key = raw_input("Please input your key for decryption: ")
-	for char in key:
-		#Convert key into a binary sequence
-		char = bin(int(binascii.hexlify(char),16))
-	#This is only here for developmental purposes
-	print key
+  text1 = raw_input("Please input your information to encrypt: ")
+  text2 = []
+  #Convert text1 into binary (text2)
+  for char in text1:
+    text2.append(bin(ord(char))[2:])
+  text2 = ''.join(text2)
+
+  key = raw_input("Please input your key for decryption: ")
+  decryptionKey = []
+  #Convert key into binary (decryptionKey)
+  for char in key:
+    decryptionKey.append(bin(ord(char))[2:])
+  decryptionKey = ''.join(decryptionKey)
+
+  print "The key is '%s'" %decryptionKey
+  print "The text is '%s'" %text2
+
+encrypt()
+
 
 
 		
