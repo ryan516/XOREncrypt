5c4ca20ae9b323e4572f9fa1497be64c9f51c409
Ryan Smith <the4swordz@gmail.com>
2013-10-07 22:57:25 -0600

    Added some minimal documentation

--- enc.py
+++ enc.py
@@ -3,11 +3,13 @@ import binascii
 def encrypt():
 	text = raw_input("Please input your information to encrypt: ")
 	for char in text:
+		#Convert text into a binary sequence
 		char = bin(int(binascii.hexlify(char),16))
 	key = raw_input("Please input your key for decryption: ")
 	for char in key:
+		#Convert key into a binary sequence
 		char = bin(int(binascii.hexlify(char),16))
-
+	#This is only here for developmental purposes
 	print key
 
 
