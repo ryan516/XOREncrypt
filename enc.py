b6dc6986092adf76cfb70fd3e875cb629e074f41
Ryan Smith <the4swordz@gmail.com>
2013-11-01 18:18:19 -0600

    Actually started working on encryption

--- enc.py
+++ enc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+import sys
 def encrypt():
   text1 = raw_input("Please input your information to encrypt: ")
   text2 = []
@@ -18,11 +19,35 @@ def encrypt():
   print "The text is '%s'" %text1
   print "The key is '%s'" %key
   userInput1 = raw_input("Is this information ok? y/n ")
-  if userInput1 == 'y' or 'Y':
+  if userInput1 == 'y':
   	print "I am encrypting your data, please hold on."
-  elif userInput1 == 'n' or 'N':
+  elif userInput1 == 'n':
   	print "Cancelled your operation."
   else:
-  	print "I didn't understand that. Please type y or n (I do not accept yes or no as an answer.)"
+  	print "I didn't understand that. Please type y or n (I do not accept yes or no as an answer, and make sure you type it in as lowercase. We should be able to fix this bug soon.)"
+
+  finalString = []
+
+  if userInput1 == 'y':
+    for i in text2:
+      j = 0
+      k = 0
+      val1 = text2[i] + decryptionKey[j]
+      if val1 == 0:
+        finalString[k] = 0
+      elif val1 == 1:
+        finalString[k] = 1
+      elif val1 == 2:
+        finalString[k] = 0
+      j += 1
+      k += 1
+    print finalString
+
+
+
+
+      
+
+
 
 encrypt()
