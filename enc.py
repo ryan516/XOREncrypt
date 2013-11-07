7f5cf13fd9fabb0218349aeb3d4bf1d71f56404d
Cameron White <cawhite@pdx.edu>
2013-11-06 22:25:30 -0800

    Updated to use more concise syntax
    The program now encrypts using a map function. Because of the cycle
    function python3 needs to be used. Also because python3 is used the
    syntax of print statements was changed and raw_input was switched
    to input.

--- enc.py
+++ enc.py
@@ -1,33 +1,25 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import sys
-def encrypt():
-  input_text = raw_input("Please input your information to encrypt: ")
+from itertools import cycle
 
-  key = raw_input("Please input your key for decryption: ")
+def encrypt():
+  input_text = input("Please input your information to encrypt: ")
+
+  key = input("Please input your key for decryption: ")
 
   #Verification String
-  print "The text is '{}'".format(input_text)
-  print "The key is '{}'".format(key)
-  userInput1 = raw_input("Is this information ok? y/n ")
+  print("The text is '{}'".format(input_text))
+  print("The key is '{}'".format(key))
+  userInput1 = input("Is this information ok? y/n ")
   if userInput1 == 'y':
-  	print "I am encrypting your data, please hold on."
+  	print("I am encrypting your data, please hold on.")
   elif userInput1 == 'n':
-  	print "Cancelled your operation."
+  	print("Cancelled your operation.")
   else:
-  	print "I didn't understand that. Please type y or n (I do not accept yes or no as an answer, and make sure you type it in as lowercase. We should be able to fix this bug soon.)"
+  	print("I didn't understand that. Please type y or n (I do not accept yes or no as an answer, and make sure you type it in as lowercase. We should be able to fix this bug soon.)")
   
-  if userInput1 == 'y':
-      chars = []
-      for i, c in enumerate(input_text):
-          chars.append(chr(ord(c) ^ ord(key[i%len(key)])))
-      
-      print(''.join(chars))
-
-
-
-
-      
-
-
+  if userInput1 == 'y': 
+      print(''.join(map(lambda a, b: chr(ord(a) ^ ord(b)),
+          input_text, cycle(key))))
 
 encrypt()
